b027d1def0a54e91b002a3aef412b35341612199
Joseph Liu <froseph@gmail.com>
2022-01-01 22:13:35 -0800

    Fix zsh clone directior?

--- .config/mr/available.d/zsh-quickstart-kit.zsh
+++ .config/mr/available.d/zsh-quickstart-kit.zsh
@@ -1,5 +1,5 @@
 # TPM plugin for tmux
 [$HOME/.zsh-quickstart-kit]
 order = 30
-checkout = git clone 'git@github.com:unixorn/zsh-quickstart-kit.git' 'zsh-quickstart-kit'
+checkout = git clone 'git@github.com:unixorn/zsh-quickstart-kit.git' '.zsh-quickstart-kit'
 post_checkout = cd $HOME/.zsh-quickstart-kit; stow --target=$HOME zsh
